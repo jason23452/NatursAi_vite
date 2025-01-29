697b0c358bc55c2e63ae0f0b39a808207d9b5126
jason23452 <andt30610@gmail.com>
2025-01-30 01:26:00 +0800

    0.2.1

--- src/component/connection.tsx
+++ src/component/connection.tsx
@@ -130,7 +130,7 @@ const Connection: React.FC = () => {
         };
 
         try {
-            const response = await fetch("/send-email", {
+            const response = await fetch("/api/send-email", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
